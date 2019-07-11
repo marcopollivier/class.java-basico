1aea64e110d5470e30a4811872ead6355b1fc6fe
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-11 10:02:03 -0300

    Adiciona primeiro exemplo

---
+++ src/main/java/aulas/javaDate/Exemplo001.java
@@ -0,0 +1,15 @@
+package aulas.javaDate;
+
+import java.util.Date;
+
+public class Exemplo001 {
+
+    public static void main(String[] args) {
+
+        Date novaData = new Date();
+
+        System.out.println(novaData);
+
+    }
+
+}
