d79021446191acdf4a5264c0406d21cac281c3cc
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-14 19:20:06 -0300

    Adiciona exemplo de equals e compareTo

---
+++ src/main/java/aulas/javaDate/Exemplo004.java
@@ -0,0 +1,42 @@
+package aulas.javaDate;
+
+import java.util.Date;
+
+/**
+ *  Exemplo de utilização dos metodos compareTo e equals
+ */
+public class Exemplo004 {
+
+    public static void main(String[] args) {
+
+        Date dataNoPassado = new Date(1513124807691L);
+        //Tue Dec 12 22:26:47 BRST 2017
+
+        Date dataNoFuturo = new Date(1613124807691L);
+        //Fri Feb 12 08:13:27 BRST 2021
+
+        Date mesmaDataNoFuturo = new Date(1613124807691L);
+        //Fri Feb 12 08:13:27 BRST 2021
+
+        /** Comparando se as datas são iguais */
+        boolean isEquals = dataNoFuturo.equals(mesmaDataNoFuturo);
+
+        System.out.println(isEquals);
+        //true
+
+        /** Comparando uma data com a outra */
+        int compareCase1 = dataNoPassado.compareTo(dataNoFuturo); //passado -> futuro
+
+        int compareCase2 = dataNoFuturo.compareTo(dataNoPassado); //futuro -> passado
+
+        int compareCase3 = dataNoFuturo.compareTo(mesmaDataNoFuturo); //datas equivalentes
+
+        System.out.println(compareCase1); // -1
+        //-1
+
+        System.out.println(compareCase2); // 1
+
+        System.out.println(compareCase3); // 0
+
+    }
+}
