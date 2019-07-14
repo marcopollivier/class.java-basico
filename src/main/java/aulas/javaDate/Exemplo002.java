37e580bed24edc7c71cf58894166b657ece9f41c
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-14 15:03:57 -0300

    Corrige timeMillis

--- src/main/java/aulas/javaDate/Exemplo002.java
+++ src/main/java/aulas/javaDate/Exemplo002.java
@@ -6,19 +6,17 @@ import java.util.Date;
  *  Exemplo de utilização do construtor com passagem de parametro em milisegundos
  */
 public class Exemplo002 {
-
     public static void main(String[] args) {
 
         Long currentTimeMillis = System.currentTimeMillis();
 
         System.out.println(currentTimeMillis);
-        // 1513124807691
+        // 1563127311564
 
         Date novaData = new Date(currentTimeMillis);
 
         System.out.println(novaData);
-        // Sun Jul 14 14:23:42 BRT 2019
+        // Sun Jul 14 15:01:51 BRT 2019
 
     }
-
 }
