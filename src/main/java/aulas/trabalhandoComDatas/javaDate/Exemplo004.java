7c497a125563c467b5267d76e6433eae8eb3c5f2
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-14 19:22:07 -0300

    Formata codigo

--- src/main/java/aulas/trabalhandoComDatas/javaDate/Exemplo004.java
+++ src/main/java/aulas/trabalhandoComDatas/javaDate/Exemplo004.java
@@ -6,23 +6,18 @@ import java.util.Date;
  *  Exemplo de utilização dos metodos compareTo e equals
  */
 public class Exemplo004 {
-
     public static void main(String[] args) {
 
-        Date dataNoPassado = new Date(1513124807691L);
-        //Tue Dec 12 22:26:47 BRST 2017
+        Date dataNoPassado = new Date(1513124807691L); //Tue Dec 12 22:26:47 BRST 2017
 
-        Date dataNoFuturo = new Date(1613124807691L);
-        //Fri Feb 12 08:13:27 BRST 2021
+        Date dataNoFuturo = new Date(1613124807691L); //Fri Feb 12 08:13:27 BRST 2021
 
-        Date mesmaDataNoFuturo = new Date(1613124807691L);
-        //Fri Feb 12 08:13:27 BRST 2021
+        Date mesmaDataNoFuturo = new Date(1613124807691L); //Fri Feb 12 08:13:27 BRST 2021
 
         /** Comparando se as datas são iguais */
         boolean isEquals = dataNoFuturo.equals(mesmaDataNoFuturo);
 
-        System.out.println(isEquals);
-        //true
+        System.out.println(isEquals); //true
 
         /** Comparando uma data com a outra */
         int compareCase1 = dataNoPassado.compareTo(dataNoFuturo); //passado -> futuro
@@ -32,7 +27,6 @@ public class Exemplo004 {
         int compareCase3 = dataNoFuturo.compareTo(mesmaDataNoFuturo); //datas equivalentes
 
         System.out.println(compareCase1); // -1
-        //-1
 
         System.out.println(compareCase2); // 1
 
