d5f9711f3984c6ace09025ac519ceb5af67b1a1d
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-14 23:30:06 -0300

    Adiciona exemplo SimpleDateFormat

---
+++ src/main/java/aulas/trabalhandoComDatas/dateFormat/Exemplo009.java
@@ -0,0 +1,22 @@
+package aulas.trabalhandoComDatas.dateFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Exemplo de formatação de data com SimpleDateFormat
+ */
+public class Exemplo009 {
+    public static void main(String[] args) {
+
+        Date agora = new Date();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        String dataFormatada = formatter.format(agora);
+
+        System.out.println(dataFormatada);
+        // 14/07/2019
+
+    }
+}
