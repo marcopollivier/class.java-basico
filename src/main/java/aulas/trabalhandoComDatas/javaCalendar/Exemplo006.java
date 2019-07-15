94c918f51aa4989efba84af0c0c25afc7bf13e99
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-07-14 21:00:02 -0300

    Adiciona manipulacao de datas com calendar

---
+++ src/main/java/aulas/trabalhandoComDatas/javaCalendar/Exemplo006.java
@@ -0,0 +1,29 @@
+package aulas.trabalhandoComDatas.javaCalendar;
+
+import java.util.Calendar;
+
+/**
+ *  Exemplo de utilização da classe Calendar
+ */
+public class Exemplo006 {
+    public static void main(String[] args) {
+
+        Calendar agora = Calendar.getInstance();
+
+        System.out.println("A data corrente é : " + agora.getTime());
+        // A data corrente é : Sun Jul 14 20:50:31 BRT 2019
+
+        agora.add(Calendar.DATE, -15);
+        System.out.println("15 dias atrás: " + agora.getTime());
+        // 15 dias atrás: Sat Jun 29 20:50:31 BRT 2019
+
+        agora.add(Calendar.MONTH, 4);
+        System.out.println("4 meses depois: " + agora.getTime());
+        // 4 meses depois: Tue Oct 29 20:50:31 BRT 2019
+
+        agora.add(Calendar.YEAR, 2);
+        System.out.println("2 anos depois: " + agora.getTime());
+        // 2 anos depois: Fri Oct 29 20:50:31 BRT 2021
+        
+    }
+}
